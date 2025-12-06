6e33290c2262c8ce115ec596a61fa2e5039c6dc8
sifat1234 <ahahmedsifat111@gmail.com>
2025-12-06 22:05:31 +0600

    Verify OTP API

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,11 @@
 import generateOtp from '../utils/otpGenerator.js';
 import User from '../models/user.model.js';
 import { response } from '../utils/response.js';
+import { sendOtpEmail } from '../services/email.service.js';
+import {
+  sendOtpToPhoneNumber,
+  verifyOtpCode,
+} from '../services/twilio.service.js';
 
 const sendOtp = async (req, res) => {
   const { phoneNumber, phoneSuffix, email } = req.body;
@@ -16,6 +21,7 @@ const sendOtp = async (req, res) => {
       }
       user.emailOtp = otp;
       user.emailOtpExpiry = expiry;
+      await sendOtpEmail(email, otp);
       await user.save();
 
       return response(res, 200, 'OTP send to your email', { email });
@@ -32,6 +38,7 @@ const sendOtp = async (req, res) => {
       user = new User({ phoneNumber: fullPhoneNumber, phoneSuffix });
     }
 
+    await sendOtpToPhoneNumber(fullPhoneNumber);
     await user.save();
 
     return response(res, 200, 'OTP sent successfully', {
@@ -41,4 +48,46 @@ const sendOtp = async (req, res) => {
     console.error('Error sending OTP:', error);
     return response(res, 500, 'Internal server error');
   }
+};
+
+const verifyOtp = async (req, res) => {
+  const { phoneNumber, phoneSuffix, otp, email } = req.body;
+  try {
+    let user;
+    if (email) {
+      user = await User.findOne({ email });
+      if (!user) {
+        return response(res, 400, 'User not found');
+      }
+      const now = new Date();
+      if (
+        !user.emailOtp ||
+        String(user.emailOtp) !== String(otp) ||
+        now > user.emailOtpExpiry
+      ) {
+        return response(res, 400, 'Invalid or expired OTP');
+      }
+
+      user.isVerified = true;
+      user.emailOtp = null;
+      user.emailOtpExpiry = null;
+      await user.save();
+    }
+
+    if (phoneNumber && phoneSuffix) {
+      const fullPhoneNumber = `${phoneSuffix}${phoneNumber}`;
+      user = await User.findOne({ phoneNumber: fullPhoneNumber });
+      if (!user) {
+        return response(res, 400, 'User not found');
+      }
+      // Here you would typically verify the OTP with Twilio's service
+      const result = await verifyOtpCode(fullPhoneNumber, otp);
+      if (result.status !== 'approved') {
+        return response(res, 400, 'Invalid OTP');
+      }
+
+      user.isVerified = true;
+      await user.save();
+    }
+  } catch (error) {}
 };
