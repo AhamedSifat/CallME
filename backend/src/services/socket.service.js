6472a6718de005c8bb7c4149e0d8076fdf5ec354
sifat1234 <ahahmedsifat111@gmail.com>
2025-12-13 22:15:47 +0600

    Initialize backend project with dependency management and socket service.

---
+++ backend/src/services/socket.service.js
@@ -0,0 +1,59 @@
+import {Server} from "socket.io";
+import Message from "../models/message.model.js";
+import User from "../models/user.model.js";
+
+//map to store online user ->userId, socketId
+const onlineUsers = new Map();
+
+//Map to track typing status ->userId -> [conversation]: boolean
+const typingUsers = new Map();
+
+
+const initializeSocket = (server) => {
+    const io = new Server(server, {
+      cors: {
+        origin: "*",
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        
+      },
+      pingTimeout: 60000,
+    });
+
+
+    //when a new socket connection is established
+    io.on("connection", (socket) => {
+      console.log(`a user is  connected: ${socket.id}`);
+
+      let userId= null;
+
+      //handle user connection and mark user as online
+      socket.on("user_connected", async (connectingUserId) => {
+       try {
+
+        userId= connectingUserId;
+        onlineUsers.set(userId, socket.id);
+        socket.join(userId) //join a personal room for direct emit
+
+        //update user status to online
+        await User.findByIdAndUpdate(userId, {
+          isOnline: true,
+          lastSeen: new Date()
+        });
+
+       //notify all users that this user is online
+        io.emit("user_status", {userId, isOnline: true});
+        
+       } catch (error) {
+        console.log(`error handling user connection: ${error}`);
+       }
+      });
+
+
+
+      
+     
+    });
+
+
+}
